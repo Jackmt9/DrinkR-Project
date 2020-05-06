6d83f1a4068fae4405c9798e40ceb06286f3453c
Jackmt9 <jackmt9@gmail.com>
2020-05-06 16:34:53 -0400

    Minor Bugs

--- index.js
+++ index.js
@@ -1,257 +1,257 @@
-let drinkList = document.querySelector('ul')
-let container = document.querySelector('.container')
-let createButton = document.querySelector('.create-button')
-const URL = `http://localhost:3000/drinks`
 
+const container = document.querySelector('.container')
 
-createButton.addEventListener("click", () => {
-    createNew()
-})
+const URLmain = `http://localhost:3000`
 
-makeFetch()
+makeLogin()
 
-function makeFetch(){
-    fetch("http://localhost:3000/drinks")
-    .then(r => r.json())
-    .then(obj => {
-        obj.forEach(displayDrink)
-    })
-}
+function makeLogin(){
+    emailForm = document.createElement('form')
 
-function displayDrink(drinkInfo) {
-    drinkLi = document.createElement('li')
-    drinkLi.class = "display"
-
-    drinkName = document.createElement('h2')
-    drinkName.class = 'name'
-    drinkName.innerText = drinkInfo.name
-
-    image = document.createElement('img')
-    image.setAttribute("class", "image")
-    image.src = drinkInfo.image
-    image.alt = `Photo of ${drinkInfo.name}`
-
-    drinkLi.append(image, drinkName)
-    drinkList.append(drinkLi)
-
-    liClick(drinkLi, drinkInfo)
-}
-
-function liClick(drinkLi, drinkInfo) {
-    drinkLi.addEventListener("click", () => {
-        drinkList.innerText = ''
-        makeCard(drinkInfo)
-    })
-}
-
-function makeCard(drinkInfo) {
-    drinkName = document.createElement('h2')
-    drinkName.class = 'name'
-    drinkName.innerText = drinkInfo.name
-    
-    drinkImage = document.createElement('img')
-    drinkImage.setAttribute("class", "image")
-    drinkImage.src = drinkInfo.image
-    drinkImage.alt = `Photo of ${drinkInfo.name}`
-
-    drinkIngredients = document.createElement('p')
-    drinkIngredients.class = "ingredients"
-    drinkIngredients.innerText = drinkInfo.ingredients
-
-    drinkSteps = document.createElement('p')
-    drinkSteps.class = "steps"
-    drinkSteps.innerText = drinkInfo.steps
-
-    deleteButton = document.createElement('button')
-    deleteButton.class = "delete-button"
-    deleteButton.innerText = '❌'
-    deleteClick(deleteButton, drinkInfo)
-
-    backButton = document.createElement('button')
-    backButton.class = "back-button"
-    backButton.innerText = '←'
-    backClick(backButton)
-
-    updateButton = document.createElement('button')
-    updateButton.setAttribute("class", "update-button")
-    updateButton.innerText = "Update Drink"
-    updateClick(updateButton, drinkInfo)
-
-    container.append(drinkName, drinkImage, drinkIngredients, drinkSteps, deleteButton, backButton, updateButton)
-}
-
-function deleteClick(button, drinkInfo){
-    button.addEventListener("click", () => {
-        fetch(`http://localhost:3000/drinks/${drinkInfo.id}`, {
-            method: "DELETE"
-        })
-        .then(r => r.json())
-        .then(obj => {
-            console.log(obj)
-            container.innerText = ''
-            makeFetch()
-        })
-    })
-}
-
-function backClick(backButton){
-    backButton.addEventListener("click", () => {
-        container.innerText = ''
-        makeFetch()
-    })
-}
-
-function createNew() {
-    container.innerText = ''
-    drinkList.innerText = ''
-
-    newForm = document.createElement('form')
-    newForm.setAttribute("class", "drink-form")
-    
-    form = document.createElement('form')
-    form.setAttribute("class", "new-form")
-
-    titleInput = document.createElement("input")
-    titleInput.type = 'text'
-    titleInput.name = 'title'
-    titleInput.value = ''
-    titleInput.placeholder = 'title'
-    
-    ingredientsInput = document.createElement("input")
-    ingredientsInput.type = 'text'
-    ingredientsInput.name = 'ingredients'
-    ingredientsInput.value = ''
-    ingredientsInput.placeholder = 'ingredients'
-
-    stepsInput = document.createElement("input")
-    stepsInput.type = 'text'
-    stepsInput.name = 'steps'
-    stepsInput.value = ''
-    stepsInput.placeholder = 'steps'
-
-    imageInput = document.createElement("input")
-    imageInput.type = 'text'
-    imageInput.name = 'image'
-    imageInput.value = ''
-    imageInput.placeholder = 'image'
+    input = document.createElement('input')
+    input.type = 'text'
+    input.name = 'email'
+    input.value = ''
+    input.placeholder = 'email'
 
     submit = document.createElement('button')
-    submit.innerText = "submit"
+    submit.innerText = 'submit'
 
-    form.append(titleInput, imageInput, ingredientsInput, stepsInput, submit)
-    container.append(form)
-
-    submitClick(form)
-
-}
-
-function createUpdate(info) {
-        container.innerText = ''
-        drinkList.innerText = ''
-    
-        newForm = document.createElement('form')
-        newForm.setAttribute("class", "drink-form")
-        
-        form = document.createElement('form')
-        form.setAttribute("class", "new-form")
-    
-        titleInput = document.createElement("input")
-        titleInput.type = 'text'
-        titleInput.name = 'title'
-        titleInput.value = info.name
-        
-        ingredientsInput = document.createElement("input")
-        ingredientsInput.type = 'text'
-        ingredientsInput.name = 'ingredients'
-        ingredientsInput.value = info.ingredients
-    
-        stepsInput = document.createElement("input")
-        stepsInput.type = 'text'
-        stepsInput.name = 'steps'
-        stepsInput.value = info.steps
-    
-        imageInput = document.createElement("input")
-        imageInput.type = 'text'
-        imageInput.name = 'image'
-        imageInput.value = info.image
-    
-        submit = document.createElement('button')
-        submit.innerText = "submit"
-    
-        form.append(titleInput, imageInput, ingredientsInput, stepsInput, submit)
-        container.append(form)
-    
-        listenForSubmit(form, info)
-    
-}
-
-function listenForSubmit(form, info) {
-    form.addEventListener("submit", (evt) => {
+    emailForm.addEventListener('submit', (evt) => {
         evt.preventDefault()
-        let title = form.title.value
-        let image = form.image.value
-        let ingredients = form.ingredients.value
-        let steps = form.steps.value
-
-        fetch(`http://localhost:3000/drinks/${info.id}`, {
-            method: "PATCH",
-            headers: {
-                'content-type': 'application/json',
-                "Accept": "Application/json"
-            },
-            body: JSON.stringify({
-                name: title,
-                image: image,
-                ingredients: ingredients,
-                steps: steps
-            })
-        })
-        .then(r => r.json())
-        .then(obj => {
-            console.log(obj)
-            container.innerText = ''
-            makeFetch()
-        })
-    })
-}
-
-function updateClick(button, info) {
-    button.addEventListener("click", () => {
-        createUpdate(info)
-    })
-}
-
-function submitClick(form) {
-    form.addEventListener("submit", (evt) => {
-        evt.preventDefault()
-
-        let title = evt.target.title.value
-        let image = evt.target.image.value
-        let ingredients = evt.target.ingredients.value
-        let steps = evt.target.steps.value
-
-        sendToDb(title, image, ingredients, steps)
+        helloUser(evt.target.email.value)
         evt.target.reset()
     })
+
+    emailForm.append(input, submit)
+
+    container.innerText = ''
+    container.append(emailForm)
 }
 
-function sendToDb(title, image, ingredients, steps) {
-    fetch(URL, {
+function helloUser(email) {
+    fetch(URLmain + '/users', {
         method: "POST",
         headers: {
             'content-type': 'application/json'
         },
         body: JSON.stringify({
-            name: title,
+            email: email
+        })
+    })
+    .then(r => r.json())
+    .then(user => {
+        console.log(user)
+        fetchDrinks()
+    })
+}
+
+function fetchDrinks() {
+    fetch(URLmain + '/drinks')
+    .then(r => r.json())
+    .then(drinks => {
+        container.innerText = ''
+
+        createButton = document.createElement('button')
+        createButton.innerText = 'Create Drink'
+        createButton.addEventListener('click', () => {
+            makeForm("new")
+        })
+        container.append(createButton)
+
+        drinkUl = document.createElement('ul')
+        drinks.forEach(drink => makeDrinkIndex(drink, drinkUl))
+    })
+}
+
+function makeDrinkIndex(drink, ul) {
+    li = document.createElement('li')
+
+    drinkName = document.createElement('h2')
+    drinkName.innerText = drink.name
+
+    image = document.createElement('img')
+    image.setAttribute("class", "image")
+    image.src = drink.image
+    image.alt = `Photo of ${drinkName}`
+
+    li.append(image, drinkName)
+    ul.append(li)
+    container.append(ul)
+
+    li.addEventListener('click', () => showDrinkCard(drink))
+}
+
+function showDrinkCard(drink) {
+    drinkName = document.createElement('h2')
+    drinkName.innerText = drink.name
+    
+    image = document.createElement('img')
+    image.src = drink.image
+    image.alt = `Photo of ${drink.name}`
+
+    ingredients = document.createElement('p')
+    ingredients.innerText = drink.ingredients
+
+    steps = document.createElement('p')
+    steps.innerText = drink.steps
+
+    deleteButton = document.createElement('button')
+    deleteButton.innerText = '❌'
+    deleteButton.addEventListener('click', () => {
+        deleteDrink(drink)
+    })
+
+    backButton = document.createElement('button')
+    backButton.innerText = '←'
+    backButton.addEventListener('click', () => {
+        fetchDrinks()
+    })
+
+    updateButton = document.createElement('button')
+    updateButton.innerText = "Update Drink"
+    updateButton.addEventListener('click', () => {
+        makeForm("update", drink)
+    })
+
+    likeButton = document.createElement('button')
+    likeButton.innerText = `${drink.likes.length} ❤`
+    likeButton.addEventListener('click', () => {
+        addLike(drink, likeButton)
+    })
+
+    container.innerText = ''
+    container.append(drinkName, image, ingredients, steps, deleteButton, backButton, updateButton, likeButton)
+
+}
+
+function deleteDrink(drink){
+    fetch(URLmain + `/drinks/${drink.id}`, {
+        method: "DELETE"
+    })
+    .then(r => r.json())
+    .then(obj => {
+        console.log(obj)
+        container.innerText = ''
+        fetchDrinks()
+        // needs to delete likes too- server error
+    })
+}
+
+function makeForm(action, drink={name: "", ingredients: "", steps: "", image: ""}){
+    container.innerText = ''
+
+    form = document.createElement('form')
+
+    nameInput = document.createElement("input")
+    nameInput.type = 'text'
+    nameInput.name = 'name'
+    nameInput.value = drink.name
+    nameInput.placeholder = 'Name'
+
+    ingredientsInput = document.createElement("input")
+    ingredientsInput.type = 'text'
+    ingredientsInput.name = 'ingredients'
+    ingredientsInput.value = drink.ingredients
+    ingredientsInput.placeholder = 'Ingredients'
+
+    stepsInput = document.createElement("input")
+    stepsInput.type = 'text'
+    stepsInput.name = 'steps'
+    stepsInput.value = drink.steps
+    stepsInput.placeholder = 'Steps'
+
+    imageInput = document.createElement("input")
+    imageInput.type = 'text'
+    imageInput.name = 'image'
+    imageInput.value = drink.image
+    imageInput.placeholder = 'Image'
+
+    submit = document.createElement('button')
+    submit.innerText = "submit"
+    
+    form.addEventListener('submit', (evt) => {
+        evt.preventDefault()
+
+        if (action == "update"){
+            makeUpdate(drink, form)
+        }else {
+            createNew(drink, form)
+        }
+    })
+
+    form.append(nameInput, imageInput, ingredientsInput, stepsInput, submit)
+    container.append(form)
+}
+
+function makeUpdate(drink, form) {
+    let name = form.name.value
+    let image = form.image.value
+    let ingredients = form.ingredients.value
+    let steps = form.steps.value
+
+    fetch(URLmain + `/drinks/${drink.id}`, {
+        method: "PATCH",
+        headers: {
+            'content-type': 'application/json',
+            "Accept": "Application/json"
+        },
+        body: JSON.stringify({
+            name: name,
             image: image,
             ingredients: ingredients,
             steps: steps
         })
     })
     .then(r => r.json())
+    .then(drink => {
+        console.log(drink)
+        fetchDrinks()
+    })
+}
+
+function addLike(drink, likeButton) {
+    fetch(URLmain + '/likes', {
+        method: "POST",
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify({
+            user_id: 1,
+            // need to use current user ID
+            drink_id: drink.id
+        })
+    })
+    .then(r => r.json())
     .then(obj => {
         console.log(obj)
-        container.innerText = ''
-        makeFetch()
+        likeButton.innerText = `${drink.likes.length + 1} ❤`
+        // doesnt increment after 1 click
+    })
+}
+
+function createNew(drink, form) {
+    let name = form.name.value
+    let image = form.image.value
+    let ingredients = form.ingredients.value
+    let steps = form.steps.value
+
+    fetch(URLmain + `/drinks`, {
+        method: "POST",
+        headers: {
+            'content-type': 'application/json',
+            "Accept": "Application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            image: image,
+            ingredients: ingredients,
+            steps: steps
+        })
+    })
+    .then(r => r.json())
+    .then(drink => {
+        console.log(drink)
+        fetchDrinks()
     })
 }
